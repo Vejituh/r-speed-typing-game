8ee55cd63a665c32996060d9e11c8513fb1c2965
Rafael Gomes <rafaelg1402@protonmail.com>
2021-02-15 12:38:34 +0000

    add state to textarea

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from "react"
 import './App.css'
 
 export default function App() {
+  const [textarea, setTextarea] = useState('')
   return (
     <>
       <h1>Speed Typing Game</h1>
-      <textarea name="textarea" value="" placeholder="Start typing..." />
+      <textarea name="textarea" value={textarea} placeholder="Start typing..." onChange={(e) => setTextarea(e.target.value)} />
       <h4>Amount of time remaining: </h4>
       <button>Start Game</button>
       <h1>Word Count: </h1>
