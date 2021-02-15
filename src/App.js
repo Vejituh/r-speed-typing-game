9a6c90edc82de17fbbec93eadb9765cfae1fa45d
Rafael Gomes <rafaelg1402@protonmail.com>
2021-02-15 12:27:29 +0000

    add init html for App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <h1>Speed Typing Game</h1>
+      <textarea name="textarea" value="" placeholder="Start typing..." />
+      <h4>Amount of time remaining: </h4>
+      <button>Start Game</button>
+      <h1>Word Count: </h1>
     </>
   )
 }
