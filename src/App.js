a4501e856b1b30c880d5cdb61d87aa126079e23f
Rafael Gomes <rafaelg1402@protonmail.com>
2021-02-15 12:48:18 +0000

    add calculateWordCount function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css'
 
 export default function App() {
   const [textarea, setTextarea] = useState('')
+
+  const calculateWordCount = (textarea) => {
+    const wordsArr = textarea.trim().split(" ")
+    console.log(wordsArr[0] !== ""? wordsArr.length: 0)
+  }
   return (
     <>
       <h1>Speed Typing Game</h1>
       <textarea name="textarea" value={textarea} placeholder="Start typing..." onChange={(e) => setTextarea(e.target.value)} />
       <h4>Amount of time remaining: </h4>
-      <button>Start Game</button>
+      <button onClick={() => calculateWordCount(textarea)}>Start Game</button>
       <h1>Word Count: </h1>
     </>
   )
