8c6ccc9cb05c911b4a98c5486aa4f0ba2b6d9fa4
Rafael Gomes <rafaelg1402@protonmail.com>
2021-02-16 17:28:17 +0000

    add gameState and wordCount state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import './App.css'
 
 export default function App() {
   const [textarea, setTextarea] = useState('')
+  const [timeLeft, setTimeLeft] = useState(null);
+  const [gameState, setGameState] = useState(false);
+  const [wordCount, setWordCount] = useState(0)
 
-  const calculateWordCount = (textarea) => {
-    const wordsArr = textarea.trim().split(" ")
-    console.log(wordsArr[0] !== ""? wordsArr.length: 0)
+  useEffect(() => {
+    if (timeLeft > 0) {
+      setTimeout(() => {
+      setTimeLeft(prevTimeLeft => prevTimeLeft -1)
+    },1000)
+  } else if(timeLeft === 0){
+    setGameState(false);
+    setWordCount(calculateWordCount(textarea))
   }
+  },[timeLeft])
+
+  const calculateWordCount = (words) => {
+    if (words.length > 1){
+    const wordsArr = words.trim().split(" ")
+    return wordsArr.length
+    } else {
+    return 0
+    }
+  }
+
+  const gameStart = () => {
+    if (gameState === false) {
+      setGameState(true)
+      setTimeLeft(5)
+    }
+  }
+
   return (
     <>
       <h1>Speed Typing Game</h1>
       <textarea name="textarea" value={textarea} placeholder="Start typing..." onChange={(e) => setTextarea(e.target.value)} />
-      <h4>Amount of time remaining: </h4>
-      <button onClick={() => calculateWordCount(textarea)}>Start Game</button>
-      <h1>Word Count: </h1>
+      <h4>Amount of time remaining: {timeLeft}</h4>
+      <button onClick={gameStart}>Start Game</button>
+      <h1>Word Count: {wordCount}</h1>
     </>
   )
 }
