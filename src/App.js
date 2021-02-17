94f6c801060b0758be8544205fc52ba9f42f70c2
Rafael Gomes <rafaelg1402@protonmail.com>
2021-02-17 10:44:28 +0000

    add useRef functionality to textarea

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import './App.css'
 
 export default function App() {
@@ -6,6 +6,7 @@ export default function App() {
   const [timeLeft, setTimeLeft] = useState(0);
   const [gameState, setGameState] = useState(false);
   const [wordCount, setWordCount] = useState(0)
+  const textAreaRef = useRef()
 
   useEffect(() => {
     if (timeLeft > 0) {
@@ -24,17 +25,19 @@ export default function App() {
 
   const gameStart = () => {
     if (gameState === false) {
+      setGameState(true)
       setTextarea('')
       setWordCount(0)
-      setGameState(true)
       setTimeLeft(5)
+      textAreaRef.current.disabled = false
+      textAreaRef.current.focus()
     }
   }
 
   return (
     <>
       <h1>Speed Typing Game</h1>
-      <textarea disabled={!gameState} name="textarea" value={textarea} placeholder="Start typing..." onChange={(e) => setTextarea(e.target.value)} />
+      <textarea disabled={!gameState} ref={textAreaRef} name="textarea" value={textarea} placeholder="Start typing..." onChange={(e) => setTextarea(e.target.value)} />
       <h4>Amount of time remaining: {timeLeft}</h4>
       <button disabled={gameState} onClick={gameStart}>Start Game</button>
       <h1>Word Count: {wordCount}</h1>
